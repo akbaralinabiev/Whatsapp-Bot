e4e67791906987e951b935dea0fcc40c0892c89d
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-01-07 07:38:45 +0100

    uploading

---
+++ source.py
@@ -0,0 +1,9 @@
+
+# install "pywhatkit" package in terminal
+# phone number = place here a phone number of person who you want to send a message to
+# message = place here message you want to send
+# select the hour and minute at which the message will be sent
+
+import  pywhatkit
+pywhatkit.sendwhatmsg('phone number', 'message', hour, minute) 
+
